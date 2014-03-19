c2417286c45115bdbf07dd970ae264aac00ee1f9
Vivek Rathod <vmr2117@columbia.edu>
2014-03-19 18:15:49 -0400

    finish the model rewriter

--- src/adaboost/model_rewriter.py
+++ src/adaboost/model_rewriter.py
@@ -13,24 +13,15 @@ def write_weights(adaboost_model_file, nnet_weights_file):
     adaboost_model = pickle.load(open(adaboost_model_file)) 
     nnet_weights = [np.concatenate(estimator.intercept_[:, np.newaxis],
         estimator.coef_).T for estimator in adaboost_model.estimators_]
-    nnet_weights.append(adaboost_model.estimator_weights_)
-    pickle.dump(nnet_weights, open(nnet_weights_file, 'wb'))
+    hidden_layer_wts = np.concatenate(tuple(nnet_weights), axis=0)
+    layer_weights = [hidden_layer_wts, adaboost_model.estimator_weights_]
+    pickle.dump(layer_weights, open(nnet_weights_file, 'wb'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('adaboostSAMME_model_file', help='file ')
-    wts, coefs = get_weights('../../models/numpy_array_multiclass.model')
-    np.set_printoptions(suppress=True,precision=7)
-    print 'Estimator weights:'
-    print '------------------'
-    print wts
-    print 'Coeffs:'
-    print '------------------'
-    for coef in coefs:
-        dim_1, dim_2 = np.nonzero(coef[0])
-        print coef[0][np.array(dim_1), np.array(dim_2)]
-    print 'Intercepts:'
-    print '------------------'
-    for coef in coefs:
-        dim = np.nonzero(coef[1])
-        print coef[1][np.array(dim)]
+    parser.add_argument('adaboostSAMME_model_file', help='adaboostSAMME model \
+            file')
+    parser.add_argument('nnet_weights_file', help='file to write neural net \
+    weights')
+    args = parser.parse_args()
+    write_weights(args.adaboostSAMME_model_file, args.nnet_weights_file)
