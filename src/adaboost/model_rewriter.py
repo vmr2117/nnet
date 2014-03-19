f0aa6c2c2e0cff61769f7f17bb54b84abe2996d9
Vivek Rathod <vivekmrathod@gmail.com>
2014-03-19 18:41:04 -0400

    adding model rewrited.py to the list

--- src/adaboost/model_rewriter.py
+++ src/adaboost/model_rewriter.py
@@ -11,14 +11,18 @@ import numpy as np
 
 def write_weights(adaboost_model_file, nnet_weights_file):
     adaboost_model = pickle.load(open(adaboost_model_file)) 
-    nnet_weights = [np.concatenate(estimator.intercept_[:, np.newaxis],
-        estimator.coef_).T for estimator in adaboost_model.estimators_]
+    nnet_weights = [np.concatenate((estimator.intercept_[:, np.newaxis],
+        estimator.coef_), axis = 1) for estimator in adaboost_model.estimators_]
     hidden_layer_wts = np.concatenate(tuple(nnet_weights), axis=0)
     layer_weights = [hidden_layer_wts, adaboost_model.estimator_weights_]
     pickle.dump(layer_weights, open(nnet_weights_file, 'wb'))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='''Convert adaboostSAMME model 
+            weights into neural network weights. All the parameters of the weak
+            learners form the weights for the activation units in the hidden  
+            layer and the weak learner alphas form the weights for the last 
+            layer''')
     parser.add_argument('adaboostSAMME_model_file', help='adaboostSAMME model \
             file')
     parser.add_argument('nnet_weights_file', help='file to write neural net \
