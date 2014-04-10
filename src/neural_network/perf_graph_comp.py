32239714b4eb0a5ea3c67db9cf42c88655137287
Vivek Rathod <vmr2117@columbia.edu>
2014-04-10 10:34:18 -0400

    results with the actual train and test set as described in Yann Lecun s page. The actual data was obtained from deeplearning.net

--- src/neural_network/perf_graph_comp.py
+++ src/neural_network/perf_graph_comp.py
@@ -4,14 +4,14 @@ from db_interface import db_interface
 from pylab import *
 
 
-def graph(rand_db, adaboost_db, filename, ttl):
+def graph(rand_db, adaboost_db, filename, ttl, x_lim, y_lim):
     db = db_interface(rand_db)
     rand_data = db.read()
     db = db_interface(adaboost_db)
     adaboost_data = db.read()
-    save_fig(rand_data, adaboost_data, filename, ttl)
+    save_fig(rand_data, adaboost_data, filename, ttl, x_lim, y_lim)
 
-def save_fig(rand_data, adaboost_data, filename, ttl):
+def save_fig(rand_data, adaboost_data, filename, ttl, x_lim, y_lim):
     it1 = [ entry[0] for entry in rand_data]
     it2 = [ entry[0] for entry in adaboost_data]
     iters = min(len(it1), len(it2))
@@ -24,7 +24,9 @@ def save_fig(rand_data, adaboost_data, filename, ttl):
     plot(x_axis, rand_nnet_vd_errs, 'r--', label = 'rand init Validation Error')
     plot(x_axis, adaboost_nnet_tr_errs, 'b', label = 'adaboost init Training Error')
     plot(x_axis, adaboost_nnet_vd_errs, 'k--', label = 'adaboost init Validation Error')
-    ylim([0, 0.2])
+    if x_lim : xlim([0, x_lim])
+    if y_lim : ylim([0, y_lim])
+    else: ylim([0, 0.9])
     legend()
     xlabel('Epoch')
     ylabel('Error')
@@ -41,6 +43,12 @@ if __name__ == '__main__':
                         data for adaboost initialized neural network')
     parser.add_argument('figure_file', help='graph of the data')
     parser.add_argument('title', help='figure title')
+    parser.add_argument('x_lim', nargs='?', help='graph x high limit', type = float)
+    parser.add_argument('y_lim', nargs='?', help='graph y high limit', type = float)
     args = parser.parse_args()
+    x_lim = None
+    y_lim = None
+    if args.x_lim : x_lim = args.x_lim
+    if args.y_lim : y_lim = args.y_lim
     graph(args.database_file_nnet_rand, args.database_file_nnet_adaboost,
-            args.figure_file, args.title)
+            args.figure_file, args.title, x_lim, y_lim)
